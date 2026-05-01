0a2d18e7f4198f016a7ded8b0a62f7de5b1ea16e
João Palmeiro <joaommpalmeiro@gmail.com>
2026-05-01 17:31:21 +0100

    Update UA

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const USER_AGENT = "my-projects";
+export const USER_AGENT = "My Projects (https://github.com/joaopalmeiro/my-projects)";
 
 export const BASE_GH_API_URL = "https://api.github.com";
 export const BASE_GL_API_URL = "https://gitlab.com/api/v4/";
